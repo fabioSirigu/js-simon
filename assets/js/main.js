18cfadfba88efe21a350026fcfe666ff5da286be
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-19 19:10:20 +0200

    add styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 /* Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
-// creare 5 numeri casuali e li stampo in pagina
-
-// - seleziono l'elemento della DOM
 const gridElement = document.querySelector('.grid');
 
 const cells = 5;
@@ -17,17 +14,8 @@ generateGrid(gridElement, cells, arrayNumber);
 const numbers = gridElement.innerText;
 
 
-
-
-// uso un bottone per generare le caselle
-// const generate = document.querySelector('.generate');
-
-// uso il bottone generate
-/* generate.addEventListener('click', function () { */
-/* } ) */
-
 // dopo 30 secondi i numeri scompaiono
-let seconds = 3
+let seconds = 30
 const intervalId = setInterval(countdown, 1000)
 
 function countdown() {
@@ -70,8 +58,7 @@ function numberCheck (numeriRandom, numeriPrompt){
             }
       }
 }
-// creo gli elementi nei quali voglio inserire i numeri
-// creo gli elementi della DOM con una funzione e gli inserisco i numeri
+
 function generateGrid (where, howMany, singleNumb){
       for (let i = 0; i < howMany; i++) {
             const cellElement = document.createElement('div');
